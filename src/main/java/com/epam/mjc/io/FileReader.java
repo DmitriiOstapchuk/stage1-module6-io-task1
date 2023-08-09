6cacebb24d0b6fca568a9ec6d40f97f790a0186e
Dmitrii Ostapchuk <dmit.ostapchuk@gmail.com>
2023-08-09 14:41:20 +0600

    task is completed

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 public class FileReader {
+    public static void main(String[] args) {
+        File filePath  = new File("src/main/resources/Profile.txt");
+        Profile newProfile = (new FileReader()).getDataFromFile(filePath);
+        System.out.println(newProfile.toString());
+    }
 
     public Profile getDataFromFile(File file) {
+        String name;
+        Integer age;
+        String email;
+        Long phone;
+        try (BufferedReader bf = new BufferedReader(new java.io.FileReader(file))) {
+            ArrayList <String> lines = new ArrayList<>();
+            while (bf.ready()) {
+                lines.add(bf.readLine());
+            }
+            Map <String, String> profileData = lines.stream()
+                    .map(s -> s.split(": "))
+                    .collect(Collectors.toMap(e -> e[0], e -> e[1]));
+            name = profileData.get("Name");
+            age = Integer.parseInt (profileData.get("Age"));
+            email = profileData.get("Email");;
+            phone = Long.parseLong (profileData.get("Phone"));
+            return new Profile(name, age, email, phone);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new Profile();
     }
 }
